eee7f64ce0178b95a3ee819f4ea6c2114b043d7b
Dima Filipenko <dimafilipenko2002@gmail.com>
2020-09-29 13:34:10 +0300

    first commit

---
+++ resources/js/components/WordsCloud.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="chart">
+        <word-cloud-chart
+            :data="wordCloudData"
+            :size-range="[0,50]"
+        />
+    </div>
+</template>
+
+<script>
+
+
+    // import wordsCloud from "dr-vue-echarts"
+
+    export default {
+        name: 'Base',
+        data() {
+            return {
+                wordCloudData: this.party()
+
+            }
+
+        },
+        methods: {
+            party: function () {
+                let arr = []
+                for (let i=1; i<=10; i++){
+                    arr.push({
+                        name: 'Партия '+i,
+                        value: Math.floor(Math.random() * Math.floor(100)),
+                    })
+
+                }
+                return arr
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .chart {
+        width: 100%;
+        height: 300px;
+    }
+</style>
